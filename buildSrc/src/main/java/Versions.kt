97e62eea512804acd7f2b22d75104b636bbf88ac
NICHX <1244012385@qq.com>
2026-05-17 08:41:43 +0800

    更新版本号1.0.3

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -4,5 +4,5 @@ object Versions {
     const val minSdkVersion = 31
     const val targetSdkVersion = 33
     const val versionCode = 1
-    const val versionName = "1.0.1"
+    const val versionName = "1.0.3"
 }
