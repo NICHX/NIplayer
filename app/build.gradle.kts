a14765ccdab0ac1c053e520521140234c4846dcb
NICHX <1244012385@qq.com>
2026-08-04 16:20:24 +0800

    chore(release): bump version to 2.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.nichx.niplayer"
         minSdk = 26
         targetSdk = 37
-        versionCode = 6
-        versionName = "2.2.0"
+        versionCode = 7
+        versionName = "2.2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
