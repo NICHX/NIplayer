cbbe0a1280bbbc99a5fc330b1a577d22fafabc3f
NICHX <1244012385@qq.com>
2026-09-22 09:42:14 +0800

    chore: 版本号 2.5.9(21) → 2.5.10(22)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.nichx.niplayer"
         minSdk = 26
         targetSdk = 37
-        versionCode = 21
-        versionName = "2.5.9"
+        versionCode = 22
+        versionName = "2.5.10"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
